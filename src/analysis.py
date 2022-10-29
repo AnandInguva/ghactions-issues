8aa6082335f2a09ea17fab104c763bd672b50fc6
Anand Inguva <34158215+AnandInguva@users.noreply.github.com>
2022-10-28 20:48:25 -0400

    updated code

--- src/analysis.py
+++ src/analysis.py
@@ -5,10 +5,12 @@ import json
 
 class GitHubIssues:
   def __init__(self, owner, repo):
-    self.owner = owner
-    self.repo = repo
-    self.query = 'https://api.github.com/repos/AnandInguva/{}/issues'.format(
-         self.repo)
+    # self.owner = owner
+    # self.repo = repo
+    self.query = "https://api.github.com/repos/AnandInguva/ghactions-issues/issues"
+
+    # self.query = 'https://api.github.com/repos/AnandInguva/{}/issues'.format(
+    #      self.repo)
     self._github_token = os.environ['GITHUB_TOKEN']
     self.headers = {
         "Authorization": 'token {}'.format(self._github_token),
@@ -19,33 +21,17 @@ class GitHubIssues:
           'A Github Personal Access token is required to create Github Issues.')
 
   def create_issue(self, title, description, label: str = 'bug'):
-    # current_issue = self.search_issue_with_title(title, label)
-    # if current_issue['total_count']:
-    #   issue_number = current_issue['items']['number']
-    #   print(issue_number)
-    #   raise NotImplementedError
-    # else:
       data = {
-          'owner': self.owner,
-          'repo': self.repo,
+          # 'owner': self.owner,
+          # 'repo': self.repo,
           'title': title,
-          'body': description,
-          'label': label
+          # 'body': description,
+          # 'label': label
       }
       r = requests.post(
           url=self.query, data=json.dumps(data), headers=self.headers)
       print(r.json())
-      # assert r.status_code == 201
-
-  def search_issue_with_title(self, title, label):
-    search_query = "repo:{}/{}+{} type:issue is:open label:{}".format(
-        self.owner, self.repo, title, label)
-    query = "https://api.github.com/search/issues?q={}".format(search_query)
-
-    response = requests.get(url=query, headers=self.headers)
-    print(response.json())
-    print(response.json()['total_count'])
-    return response.json()
+      print(r)
 
   def comment_on_issue(self):
     """
